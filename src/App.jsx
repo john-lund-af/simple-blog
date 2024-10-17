71dde6371304dfd17b5b9b6431c29c6764c9e971
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-17 18:49:46 +0200

    Added Router, Navbar and Pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import BlogList from './pages/BlogList';
+import About from './pages/About';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
     <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/blogs" element={<BlogList />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </>
   )
 }
